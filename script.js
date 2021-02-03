ab6da8bf3aa9ecb8c0e91eb023ffb07b66836424
Yusa Islam <yusaislam48@gmail.com>
2021-02-04 00:53:03 +0600

    added search option

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const inputLocation = document.getElementById('inputLocation');
-const apiId = '19f107ff6c2d0da705f7206ed97c731d';
 
 const submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', function(){
@@ -12,6 +11,16 @@ function getWeather(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputLocation.value+'&appid=19f107ff6c2d0da705f7206ed97c731d')
         .then(res => res.json())
         .then(data => {
-            console.log(data.weather);
+            const tempValue = Math.round(data.main.temp - 273.15);
+            document.getElementById('locationName').innerText = data.name;
+            document.getElementById('tempValue').innerText = tempValue;
+            document.getElementById('description').innerText = data.weather[0].description;
+        })
+
+        .catch(error => {
+            alert('Wrong city name!')
+            document.getElementById('locationName').innerText = 'Wrong city name!';
+            document.getElementById('tempValue').innerText = 00;
+            document.getElementById('description').innerText = '';
         })
 }
