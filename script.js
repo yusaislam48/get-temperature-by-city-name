b0452f4b0350ee84db3d62b2bc4cdedb8bc07c8d
Yusa Islam <yusaislam48@gmail.com>
2021-02-04 01:28:26 +0600

    added automatic changing icon

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const inputLocation = document.getElementById('inputLocation');
 const submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', function(){
     console.log(inputLocation.value);
-    getWeather();
+    getWeather(inputLocation);
 })
 
 
-function getWeather(){
+function getWeather(inputLocation){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputLocation.value+'&appid=19f107ff6c2d0da705f7206ed97c731d')
         .then(res => res.json())
         .then(data => {
